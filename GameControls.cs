c66b34d88c0d3aec0c0d20675c6f66ca5a6c195c
jfcarocota <jfcarocota@gmail.com>
2021-10-29 08:22:59 -0700

    changes

--- GameControls.cs
+++ GameControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/GameControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Packages/com.cglab.gamelibs/GameControls.inputactions'
 
 using System;
 using System.Collections;
